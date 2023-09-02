e5fc4de5f6b698e897d3cd53308f87f5d43f522e
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-02 12:04:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
+
+//variable
+let myVariable = "I am Victor";
+
+//operators
+//this are maths operators...
+
+//conditionals
+
+let iceCream = "chocolate";
+if (iceCream === "chocolate") {
+  alert("Yay, I love chocolate ice cream!");
+} else {
+  alert("Awwww, but chocolate is my favorite…");
+}
